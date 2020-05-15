61f462823a3f1bafb66b5eaeac7013074875ed2f
Daniel Williamson <danielwilliamson@Daniels-MBP.lan>
2020-05-15 12:14:41 +0100

    A fix for saving and loading with a demonstration

--- Demonstrations/SavingBug.R
+++ Demonstrations/SavingBug.R
@@ -12,3 +12,24 @@ save(TestEm, file="TestEm.RData")
 rm(TestEm)
 load("TestEm.RData")
 print(TestEm[[1]])
+
+TestEm <- BuildNewEmulators(tData, HowManyEmulators = 2)
+save(TestEm, file="TestEm.RData")
+py_save_object(TestEm$mogp,"TestEm_mogp")
+rm(TestEm)
+load("TestEm.RData")
+TestEm$mogp <- py_load_object("TestEm_mogp")
+newDesign <- 2*randomLHS(100,3)-1
+preds <- TestEm$mogp$predict(newDesign, deriv=FALSE)
+
+####FIXED: New saving functions in BuildEmulator.R
+
+
+TestEm <- BuildNewEmulators(tData, HowManyEmulators = 2)
+save_ExUQmogp(TestEm, filename = "TestEm")
+rm(TestEm)
+TestEm <- load_ExUQmogp("TestEm")
+
+newDesign <- 2*randomLHS(100,3)-1
+preds <- TestEm$mogp$predict(newDesign, deriv=FALSE)
+
